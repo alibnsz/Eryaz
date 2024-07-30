86b266876b5e0935c9760cb13cddad5f6eb44453
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2024-07-30 10:30:54 +0300

    Initial Commit

---
+++ Eryaz/EryazApp.swift
@@ -0,0 +1,17 @@
+//
+//  EryazApp.swift
+//  Eryaz
+//
+//  Created by Mehmet Ali Bunsuz on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct EryazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
